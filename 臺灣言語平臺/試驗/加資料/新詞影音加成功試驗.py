364d6c83d67e773b9baa322a8134cf9e9de9f8ac
薛丞宏 <ihcaoe@gmail.com>
2015-03-03 18:07:34 +0800

    補足加影音的欄位 丞宏

--- 臺灣言語平臺/試驗/加資料/新詞影音加成功試驗.py
+++ 臺灣言語平臺/試驗/加資料/新詞影音加成功試驗.py
@@ -28,21 +28,21 @@ class 新詞影音加成功試驗(資料庫試驗):
 			音檔.setnchannels(1)
 			音檔.setframerate(16000)
 			音檔.setsampwidth(2)
-			音檔.writeframesraw(b'0' * 100)
+			音檔.writeframesraw(b'sui2' * 20)
 	def tearDown(self):
 		pass
 	def test_一般參數(self):
 		self.client.login()
 		回應 = self.client.post(
 			'/加資料/新詞影音', {  # 全部都必須字串形態
-				'外語請教條編號':self.外語請教條編號,
+				'外語請教條編號':self.外語請教條編號,  # 針對哪一個外語請教條的母語影音
 				'來源':{'名':'阿媠', '職業':'學生'},  # 一定要有「名」，其餘資訊視情況增加
 				'種類':'字詞',  # 必須愛和外語請教條的種類一樣
 				'語言腔口':'閩南語',  # 必須愛和外語請教條的語言腔口一樣
 				'著作所在地':'花蓮',  # 不設限，隨意增加
 				'著作年':'2014',  # 不設限，隨意增加
-				'屬性':{'詞性':'形容詞', '字數':'2'},  # 不設限，隨意增減
-				'影音資料':self.檔案,  # 檔案型態
+				'屬性':{'詞性':'形容詞', '字數':'1'},  # 不設限，隨意增減
+				'影音資料':self.檔案,  # 錄製的影音檔，檔案型態
 			}
 		)
 # 		前端回傳結果
@@ -68,19 +68,20 @@ class 新詞影音加成功試驗(資料庫試驗):
 		self.assertEqual(影音.著作年, self.二空一四)
 		self.assertEqual(影音.屬性.count(), 2)
 		self.assertEqual(影音.屬性.get(分類='詞性').性質, '形容詞')
-		self.assertEqual(影音.屬性.get(分類='字數').性質, '2')
-		self.assertEqual(影音.影音資料, '漂亮')
+		self.assertEqual(影音.屬性.get(分類='字數').性質, '1')
+		self.assertEqual(影音.原始影音資料.read(), self.檔案.getvalue())
 	def test_來源自己(self):
 		self.client.login()
 		回應 = self.client.post(
 			'/加請教條', {
+				'外語請教條編號':self.外語請教條編號,  # 針對哪一個外語請教條的母語影音
 				'來源':'自己',  # 可用「自己」，會把來源指向自己
 				'種類':'字詞',
 				'語言腔口':'閩南語',
 				'著作所在地':'花蓮',
 				'著作年':'2014',
-				'屬性':{'詞性':'形容詞', '字數':'2'},
-				'影音資料':'漂亮',
+				'屬性':{'詞性':'形容詞', '字數':'1'},
+				'影音資料':self.檔案,  # 錄製的影音檔，檔案型態
 			}
 		)
 # 		前端回傳結果
@@ -103,19 +104,20 @@ class 新詞影音加成功試驗(資料庫試驗):
 		self.assertEqual(影音.著作年, self.二空一四)
 		self.assertEqual(影音.屬性.count(), 2)
 		self.assertEqual(影音.屬性.get(分類='詞性').性質, '形容詞')
-		self.assertEqual(影音.屬性.get(分類='字數').性質, '2')
-		self.assertEqual(影音.影音資料, '漂亮')
+		self.assertEqual(影音.屬性.get(分類='字數').性質, '1')
+		self.assertEqual(影音.原始影音資料.read(), self.檔案.getvalue())
 	def test_來源名自己(self):
 		self.client.login()
 		回應 = self.client.post(
 			'/加請教條', {
+				'外語請教條編號':self.外語請教條編號,  # 針對哪一個外語請教條的母語影音
 				'來源':{'名':'自己'},  # 當作一个人叫做「自己」
 				'種類':'字詞',
 				'語言腔口':'閩南語',
 				'著作所在地':'花蓮',
 				'著作年':'2014',
-				'屬性':{'詞性':'形容詞', '字數':'2'},
-				'影音資料':'漂亮',
+				'屬性':{'詞性':'形容詞', '字數':'1'},
+				'影音資料':self.檔案,  # 錄製的影音檔，檔案型態
 			}
 		)
 # 		前端回傳結果
@@ -139,5 +141,5 @@ class 新詞影音加成功試驗(資料庫試驗):
 		self.assertEqual(影音.著作年, self.二空一四)
 		self.assertEqual(影音.屬性.count(), 2)
 		self.assertEqual(影音.屬性.get(分類='詞性').性質, '形容詞')
-		self.assertEqual(影音.屬性.get(分類='字數').性質, '2')
-		self.assertEqual(影音.影音資料, '漂亮')
+		self.assertEqual(影音.屬性.get(分類='字數').性質, '1')
+		self.assertEqual(影音.原始影音資料.read(), self.檔案.getvalue())
