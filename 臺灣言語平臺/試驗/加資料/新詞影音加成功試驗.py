9e7d21008fb14af223e7799453b89c8cc78901e2
薛丞宏 <ihcaoe@gmail.com>
2015-03-04 21:02:43 +0800

    新詞可以重覆，試驗設計好 fix #6 丞宏

--- 臺灣言語平臺/試驗/加資料/新詞影音加成功試驗.py
+++ 臺灣言語平臺/試驗/加資料/新詞影音加成功試驗.py
@@ -158,3 +158,59 @@ class 新詞影音加成功試驗(資料庫試驗):
 		self.assertEqual(影音.屬性.get(分類='詞性').性質, '形容詞')
 		self.assertEqual(影音.屬性.get(分類='字數').性質, '1')
 		self.assertEqual(影音.原始影音資料.read(), self.檔案.getvalue())
+	def test_仝款資料加兩擺(self):
+		'影音比較的成本太大，所以不檢查'
+		self.client.login()
+		self.client.post(
+			'/加資料/新詞影音', {
+				'外語請教條項目編號':self.外語請教條項目編號,
+				'來源':{'名':'阿媠', '職業':'學生'},
+				'種類':'字詞',
+				'語言腔口':'閩南語',
+				'著作所在地':'花蓮',
+				'著作年':'2014',
+				'屬性':{'詞性':'形容詞', '字數':'1'},
+				'影音資料':self.檔案,
+			}
+		)
+		self.影音表資料數 = 影音表.objects.conut()
+		回應 = self.client.post(
+			'/加資料/新詞影音', {
+				'外語請教條項目編號':self.外語請教條項目編號,
+				'來源':{'名':'阿媠', '職業':'學生'},
+				'種類':'字詞',
+				'語言腔口':'閩南語',
+				'著作所在地':'花蓮',
+				'著作年':'2014',
+				'屬性':{'詞性':'形容詞', '字數':'1'},
+				'影音資料':self.檔案,
+			}
+		)
+# 		前端回傳結果
+		self.assertEqual(回應.status_code, 200)
+		回應資料 = json.loads(回應.content)
+		self.assertEqual(回應資料, {
+				'結果':'成功',
+				'平臺項目編號':回應資料['平臺項目編號'],
+		})
+# 		後端資料庫檢查
+		self.assertEqual(外語表.objects.conut(), self.外語表資料數)
+		self.assertEqual(影音表.objects.conut(), self.影音表資料數 + 2)
+		self.assertEqual(翻譯影音表.objects.conut(), self.翻譯影音表資料數 + 2)
+		編號 = int(回應資料['平臺項目編號'])
+		影音 = 平臺項目表.objects.get(pk=編號).影音
+		self.外語.翻譯影音.get(影音=影音)  # 確定有建立關係
+		self.assertEqual(影音.收錄者, self.臺灣人)
+		self.assertEqual(影音.來源.名, '阿媠')
+		self.assertEqual(影音.來源.屬性.count(), 1)
+		self.assertEqual(影音.來源.屬性.get().分類, '職業')
+		self.assertEqual(影音.來源.屬性.get().性質, '學生')
+		self.assertEqual(影音.版權, self.會使公開)
+		self.assertEqual(影音.種類, self.字詞)
+		self.assertEqual(影音.語言腔口, self.閩南語)
+		self.assertEqual(影音.著作所在地, self.花蓮)
+		self.assertEqual(影音.著作年, self.二空一四)
+		self.assertEqual(影音.屬性.count(), 2)
+		self.assertEqual(影音.屬性.get(分類='詞性').性質, '形容詞')
+		self.assertEqual(影音.屬性.get(分類='字數').性質, '1')
+		self.assertEqual(影音.原始影音資料.read(), self.檔案.getvalue())
--- 臺灣言語平臺/試驗/加資料/新詞文本加成功試驗.py
+++ 臺灣言語平臺/試驗/加資料/新詞文本加成功試驗.py
@@ -184,4 +184,61 @@ class 加新詞文本試驗(資料庫試驗):
 		self.assertEqual(文本.屬性.get(分類='詞性').性質, '形容詞')
 		self.assertEqual(文本.屬性.get(分類='字數').性質, '1')
 		self.assertEqual(文本.文本資料, '媠')
-
+	def test_仝款資料加兩擺(self):
+		'不同人校對的結果可能一樣，所以不檢查重覆文本'
+		self.client.login()
+		self.client.post(
+			'/加資料/新詞文本', {
+				'新詞影音項目編號':self.新詞影音項目編號,
+				'來源':{'名':'阿媠', '職業':'學生'},
+				'種類':'字詞',
+				'語言腔口':'閩南語',
+				'著作所在地':'花蓮',
+				'著作年':'2014',
+				'屬性':{'詞性':'形容詞', '字數':'1'},
+				'文本資料':'媠',
+			}
+		)
+		self.文本表資料數 = 文本表.objects.conut()
+		回應 = self.client.post(
+			'/加資料/新詞文本', {  # 全部都必須字串形態
+				'新詞影音項目編號':self.新詞影音項目編號,  # 針對哪一個外語請教條的母語文本
+				'來源':{'名':'阿媠', '職業':'學生'},  # 一定要有「名」，其餘資訊視情況增加
+				'種類':'字詞',  # 必須愛和外語請教條的種類一樣
+				'語言腔口':'閩南語',  # 必須愛和外語請教條的語言腔口一樣
+				'著作所在地':'花蓮',  # 不設限，隨意增加
+				'著作年':'2014',  # 不設限，隨意增加
+				'屬性':{'詞性':'形容詞', '字數':'1'},  # 不設限，隨意增減
+				'文本資料':'媠',  # 錄製的文本檔，檔案型態
+			}
+		)
+# 		前端回傳結果
+		self.assertEqual(回應.status_code, 200)
+		回應資料 = json.loads(回應.content)
+		self.assertEqual(回應資料, {
+				'結果':'成功',
+				'平臺項目編號':回應資料['平臺項目編號'],
+		})
+# 		後端資料庫檢查
+		self.assertEqual(外語表.objects.conut(), self.外語表資料數)
+		self.assertEqual(影音表.objects.conut(), self.影音表資料數)
+		self.assertEqual(翻譯影音表.objects.conut(), self.翻譯影音表資料數)
+		self.assertEqual(文本表.objects.conut(), self.文本表資料數 + 2)
+		self.assertEqual(影音文本表.objects.conut(), self.影音文本表資料數 + 2)
+		編號 = int(回應資料['平臺項目編號'])
+		文本 = 平臺項目表.objects.get(pk=編號).文本
+		self.外語.翻譯文本.get(文本=文本)  # 確定有建立關係
+		self.assertEqual(文本.收錄者, self.臺灣人)
+		self.assertEqual(文本.來源.名, '阿媠')
+		self.assertEqual(文本.來源.屬性.count(), 1)
+		self.assertEqual(文本.來源.屬性.get().分類, '職業')
+		self.assertEqual(文本.來源.屬性.get().性質, '學生')
+		self.assertEqual(文本.版權, self.會使公開)
+		self.assertEqual(文本.種類, self.字詞)
+		self.assertEqual(文本.語言腔口, self.閩南語)
+		self.assertEqual(文本.著作所在地, self.花蓮)
+		self.assertEqual(文本.著作年, self.二空一四)
+		self.assertEqual(文本.屬性.count(), 2)
+		self.assertEqual(文本.屬性.get(分類='詞性').性質, '形容詞')
+		self.assertEqual(文本.屬性.get(分類='字數').性質, '1')
+		self.assertEqual(文本.文本資料, '媠')
