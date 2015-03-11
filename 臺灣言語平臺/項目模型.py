3adc95d409b9f0b6131d671f1742696e653b3d9d
薛丞宏 <ihcaoe@gmail.com>
2015-03-11 08:23:55 +0800

    補轉來null，進前是拍做NULL 丞宏

--- 臺灣言語平臺/項目模型.py
+++ 臺灣言語平臺/項目模型.py
@@ -7,10 +7,10 @@ from 臺灣言語資料庫.資料模型 import 聽拍表
 
 class 平臺項目表(models.Model):
 	項目名 = '平臺項目'
-	外語 = models.ForeignKey(外語表, related_name=項目名)
-	影音 = models.ForeignKey(影音表, related_name=項目名)
-	文本 = models.ForeignKey(文本表, related_name=項目名)
-	聽拍 = models.ForeignKey(聽拍表, related_name=項目名)
+	外語 = models.ForeignKey(外語表, null=True, related_name=項目名)
+	影音 = models.ForeignKey(影音表, null=True, related_name=項目名)
+	文本 = models.ForeignKey(文本表, null=True, related_name=項目名)
+	聽拍 = models.ForeignKey(聽拍表, null=True, related_name=項目名)
 	是資料源頭 = models.BooleanField(default=False)
 	def 編號(self):
 		return self.pk
