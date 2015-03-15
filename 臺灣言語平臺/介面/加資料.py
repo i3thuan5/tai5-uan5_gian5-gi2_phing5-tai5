db7e0f919ad9e58425f95889195f5d4f6f2fa09d
薛丞宏 <ihcaoe@gmail.com>
2015-03-15 19:47:21 +0800

    賟仝款資料佮屬性無轉json的試驗猶未過 仝款資料愛佇資料庫彼爿檢查 屬性無轉，愛予資料料還原rollback 丞宏

--- 臺灣言語平臺/介面/加資料.py
+++ 臺灣言語平臺/介面/加資料.py
@@ -2,6 +2,8 @@
 from django.http.response import JsonResponse
 from 臺灣言語資料庫.資料模型 import 外語表
 import json
+from django.utils.datastructures import MultiValueDictKeyError
+from 臺灣言語資料庫.資料模型 import 種類表
 
 _自己json字串=json.dumps('自己')
 def 加外語請教條(request):
@@ -14,17 +16,48 @@ def 加外語請教條(request):
 				'外語語言',
 				'外語資料', ]
 	內容 = {}
-	for 欄位 in 欄位表:
-		內容[欄位] = request.POST[欄位]
-# 	內容['屬性']=json.loads(request.POST['屬性'])
-	內容['版權'] = '會使公開'
-	內容['收錄者'] = 1
-	if 內容['來源'] == _自己json字串:
-		內容['來源'] = 內容['收錄者']
-	print(內容)
-	外語 = 外語表.加資料(內容)
-	平臺項目 = 外語.平臺項目.create(是資料源頭=True)
-	return JsonResponse({
-		'結果':'成功',
-		'平臺項目編號':str(平臺項目.編號()),
+	try:
+		for 欄位 in 欄位表:
+			內容[欄位] = request.POST[欄位]
+			if not isinstance(內容[欄位],str):
+				return JsonResponse({
+					'結果':'失敗',
+					'原因':'資料全部一般欄位必須都是字串',
+				})
+	# 	內容['屬性']=json.loads(request.POST['屬性'])
+		內容['版權'] = '會使公開'
+		內容['收錄者'] = 1
+		if 內容['來源'] == _自己json字串:
+			內容['來源'] = 內容['收錄者']
+	# 	print(type(內容),內容)
+		try:
+			外語 = 外語表.加資料(內容)
+		except:
+			raise
+		平臺項目 = 外語.平臺項目.create(是資料源頭=True)
+	except ValueError:
+		return JsonResponse({
+			'結果':'失敗',
+			'原因':'來源抑是屬性無轉json字串',
 		})
+	except MultiValueDictKeyError:
+		return JsonResponse({
+			'結果':'失敗',
+			'原因':'資料欄位有缺',
+		})
+	except KeyError:
+		return JsonResponse({
+			'結果':'失敗',
+			'原因':'來源沒有「名」的欄位',
+		})
+	except 種類表.DoesNotExist:
+		return JsonResponse({
+			'結果':'失敗',
+			'原因':'種類欄位不符規範',
+		})
+	else:
+		return JsonResponse({
+			'結果':'成功',
+			'平臺項目編號':str(平臺項目.編號()),
+		})
+	
