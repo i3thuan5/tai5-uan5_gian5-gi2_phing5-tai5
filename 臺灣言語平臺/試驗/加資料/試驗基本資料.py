7284846e82b7aeb533f87afb16a9f9cf3f3207c6
薛丞宏 <ihcaoe@gmail.com>
2015-03-19 20:20:19 +0800

    補試驗的格式 丞宏

--- 臺灣言語平臺/試驗/加資料/試驗基本資料.py
+++ 臺灣言語平臺/試驗/加資料/試驗基本資料.py
@@ -7,6 +7,9 @@ from 臺灣言語資料庫.資料模型 import 種類表
 from 臺灣言語資料庫.欄位資訊 import 字詞
 from 臺灣言語資料庫.欄位資訊 import 語句
 from 臺灣言語資料庫.資料模型 import 來源表
+from 臺灣言語資料庫.資料模型 import 語言腔口表
+from 臺灣言語資料庫.資料模型 import 著作所在地表
+from 臺灣言語資料庫.資料模型 import 著作年表
 
 class 試驗基本資料(TestCase):
 	def setUp(self):
@@ -15,6 +18,11 @@ class 試驗基本資料(TestCase):
 		self.字詞 = 種類表.objects.create(種類=字詞)
 		self.語句 = 種類表.objects.create(種類=語句)
 		self.鄉民 = 來源表. 加來源({"名":'鄉民', '出世年':'1950', '出世地':'臺灣', })
+		
+		self.閩南語 = 語言腔口表.objects.create(語言腔口='閩南語')
+		self.華語 = 語言腔口表.objects.create(語言腔口='華語')
+		self.花蓮 = 著作所在地表.objects.create(著作所在地='花蓮')
+		self.二空一四 = 著作年表.objects.create(著作年='2014')
 
 	def tearDown(self):
 		pass
