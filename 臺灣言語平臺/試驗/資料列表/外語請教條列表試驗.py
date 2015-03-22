ef2ab78863e198aea20a54fb613ad95d0f0b68b0
薛丞宏 <ihcaoe@gmail.com>
2015-03-22 10:06:40 +0800

    改掉試驗毋著的所在 丞宏

--- 臺灣言語平臺/試驗/資料列表/外語請教條列表試驗.py
+++ 臺灣言語平臺/試驗/資料列表/外語請教條列表試驗.py
@@ -2,10 +2,16 @@
 from django.test import TestCase
 import json
 from 臺灣言語平臺.項目模型 import 平臺項目表
+from 臺灣言語資料庫.資料模型 import 來源表
+from 臺灣言語資料庫.資料模型 import 版權表
+from 臺灣言語資料庫.欄位資訊 import 字詞
+from 臺灣言語資料庫.資料模型 import 種類表
 
 class 外語請教條列表試驗(TestCase):
-	def setUp(self):
-		pass
+	def setUp(self):		
+		self.會使公開 = 版權表.objects.create(版權='會使公開')
+		self.字詞 = 種類表.objects.create(種類=字詞)
+		self.鄉民 = 來源表. 加來源({"名":'鄉民', '出世年':'1950', '出世地':'臺灣', })
 
 	def tearDown(self):
 		pass
@@ -18,7 +24,7 @@ class 外語請教條列表試驗(TestCase):
 		self.assertEqual(回應資料, [])
 		
 	def test_一个外語請教條(self):
-		水母編號 = self.加外語請教條('水母')
+		水母編號 = self.資料庫加外語請教條('水母')
 		回應 = self.client.get('/外語請教條列表')
 # 		前端回傳結果
 		self.assertEqual(回應.status_code, 200)
@@ -34,8 +40,8 @@ class 外語請教條列表試驗(TestCase):
 		])
 	
 	def test_兩个外語請教條(self):
-		水母編號 = self.加外語請教條('水母')
-		水母腦編號 = self.加外語請教條('水母腦')
+		水母編號 = self.資料庫加外語請教條('水母')
+		水母腦編號 = self.資料庫加外語請教條('水母腦')
 		回應 = self.client.get('/外語請教條列表')
 # 		前端回傳結果
 		self.assertEqual(回應.status_code, 200)
@@ -59,7 +65,9 @@ class 外語請教條列表試驗(TestCase):
 	
 	def 資料庫加外語請教條(self, 外語詞):
 		return 平臺項目表.加外語資料({
+					'收錄者':self.鄉民.編號(),
 					'來源':json.dumps({'名':'阿媠', '職業':'學生'}),
+					'版權': '會使公開',
 					'種類':'字詞',
 					'語言腔口':'閩南語',
 					'著作所在地':'花蓮',
