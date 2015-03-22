8d04fd487c7600d196315441a8de3bf71358158a
薛丞宏 <ihcaoe@gmail.com>
2015-03-22 11:06:48 +0800

    實作一部份揣外語請教條 丞宏

--- 臺灣言語平臺/網址.py
+++ 臺灣言語平臺/網址.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
+
+
 from 臺灣言語平臺.介面.加資料 import 加外語請教條
 from 臺灣言語平臺.介面.加資料 import 加新詞影音
 from 臺灣言語平臺.介面.加資料 import 加新詞文本
 from 臺灣言語平臺.介面.資料列表 import 外語請教條列表
+from 臺灣言語平臺.介面.揣外語請教條 import 揣外語請教條
 
 urlpatterns = patterns('',
 	url(r'^加資料/外語請教條$', 加外語請教條, name='加外語請教條'),
@@ -11,5 +14,5 @@ urlpatterns = patterns('',
 	url(r'^加資料/新詞文本$', 加新詞文本, name='加新詞文本'),
 	
 	url(r'^列表/外語請教條', 外語請教條列表, name='外語請教條列表'),
-	
+	url(r'^揣/外語請教條', 揣外語請教條, name='揣外語請教條'),
 )
