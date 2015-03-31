6f34ed955700aa50e823e6c222c2ac480bd3eeb1
薛丞宏 <ihcaoe@gmail.com>
2015-03-31 14:35:50 +0800

    使用者一定愛有來源佮email 丞宏

--- 臺灣言語平臺/試驗/模型/test使用者表試驗.py
+++ 臺灣言語平臺/試驗/模型/test使用者表試驗.py
@@ -15,6 +15,12 @@ class 使用者表試驗(TestCase):
 		來源內容 = {"名":'鄉民', '出世年':'1950', '出世地':'臺灣', }
 		使用者 = 使用者表.加使用者('sui2@pigu.tw', 來源內容,)
 		self.assertEqual(使用者.來源.編號(), 使用者.編號())
+	def test_愛有email(self):
+		來源內容 = {"名":'鄉民', '出世年':'1950', '出世地':'臺灣', }
+		來源 = 來源表. 加來源(來源內容)
+		self.assertRaises(IntegrityError, 使用者表.objects.create, 來源=來源,)
+	def test_愛有來源(self):
+		self.assertRaises(IntegrityError, 使用者表.objects.create, email='sui2@pigu.tw',)
 	def test_重覆email(self):
 		來源內容 = {"名":'鄉民', '出世年':'1950', '出世地':'臺灣', }
 		使用者表.加使用者('sui2@pigu.tw', 來源內容,)
