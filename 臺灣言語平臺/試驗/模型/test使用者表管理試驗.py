ea27bce916247f69b53f1eabdd4a3287fc854277
薛丞宏 <ihcaoe@gmail.com>
2015-04-08 20:19:09 +0800

    重構使用者表管理的試驗 fix #18 丞宏

--- 臺灣言語平臺/試驗/模型/test使用者表管理試驗.py
+++ 臺灣言語平臺/試驗/模型/test使用者表管理試驗.py
@@ -4,20 +4,17 @@ from 臺灣言語平臺.使用者模型 import 使用者表管理
 
 class 使用者表管理試驗(TestCase):
 	def setUp(self):
-		self.管理 = 使用者表管理()
+		管理 = 使用者表管理()
+		self.使用者 = 管理.create_superuser('sui2@pigu.tw', 'I\'m sui2')
 	def tearDown(self):
 		pass
 	def test_檢查email(self):
-		使用者 = self.管理.create_superuser('sui2@pigu.tw', 'I\'m sui2')
-		self.assertEqual(使用者.email, 'sui2@pigu.tw')
+		self.assertEqual(self.使用者.email, 'sui2@pigu.tw')
 	def test_檢查名(self):
-		使用者 = self.管理.create_superuser('sui2@pigu.tw', 'I\'m sui2')
-		self.assertEqual(使用者.來源.名, 'sui2@pigu.tw')
+		self.assertEqual(self.使用者.來源.名, 'sui2@pigu.tw')
 	def test_檢查密碼愛hash(self):
-		使用者 = self.管理.create_superuser('sui2@pigu.tw', 'I\'m sui2')
-		self.assertNotEqual(使用者.password, '')
-		self.assertNotEqual(使用者.password, 'I\'m sui2')
+		self.assertNotEqual(self.使用者.password, '')
+		self.assertNotEqual(self.使用者.password, 'I\'m sui2')
 	def test_檢查管理員(self):
-		使用者 = self.管理.create_superuser('sui2@pigu.tw', 'I\'m sui2')
-		self.assertTrue(使用者.is_staff)
-		
+		self.assertTrue(self.使用者.is_staff)
+	
