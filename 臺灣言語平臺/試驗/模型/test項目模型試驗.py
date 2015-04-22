119a31f7915c88c7ea4889b93a8962194602c36f
薛丞宏 <ihcaoe@gmail.com>
2015-04-22 14:23:02 +0800

    加揣外語的試驗 fix #13 丞宏

--- 臺灣言語平臺/試驗/模型/test項目模型試驗.py
+++ 臺灣言語平臺/試驗/模型/test項目模型試驗.py
@@ -9,6 +9,7 @@ from 臺灣言語資料庫.資料模型 import 版權表
 from 臺灣言語資料庫.欄位資訊 import 字詞
 from 臺灣言語資料庫.資料模型 import 種類表
 from 臺灣言語資料庫.資料模型 import 文本表
+from django.core.exceptions import ObjectDoesNotExist
 
 class 項目模型試驗(TestCase):
 	def setUp(self):
@@ -63,11 +64,11 @@ class 項目模型試驗(TestCase):
 		pass
 
 	def test_編號(self):
-		平臺項目=平臺項目表.加外語資料(self.外語內容)
+		平臺項目 = 平臺項目表.加外語資料(self.外語內容)
 		self.assertEqual(平臺項目.編號(), 平臺項目.pk,)
 		
 	def test_揣編號(self):
-		平臺項目=平臺項目表.加外語資料(self.外語內容)
+		平臺項目 = 平臺項目表.加外語資料(self.外語內容)
 		self.assertEqual(平臺項目表.揣編號(平臺項目.編號()), 平臺項目)
 	
 	def test_資料外語(self):
@@ -83,9 +84,26 @@ class 項目模型試驗(TestCase):
 		文本項目 = 平臺項目表.影音寫文本(影音項目.編號(), self.文本內容)
 		self.assertEqual(文本項目.資料(), 文本項目.文本)
 	def test_資料攏無(self):
-		攏無=平臺項目表.objects.create()
+		攏無 = 平臺項目表.objects.create()
 		self.assertRaises(RuntimeError, 攏無.資料)
 	def test_資料攏有濟个(self):
 		濟个項目 = 平臺項目表.加外語資料(self.外語內容)
-		濟个項目.文本=文本表.加資料(self.文本內容)
+		濟个項目.文本 = 文本表.加資料(self.文本內容)
 		self.assertRaises(RuntimeError, 濟个項目.資料)
+	
+	def test_找外語資料揣有(self):
+		外語項目 = 平臺項目表.加外語資料(self.外語內容)
+		找到的外語項目 = 平臺項目表.找外語資料(self.外語內容)
+		self.assertEqual(外語項目, 找到的外語項目)	
+	def test_找外語資料種類表揣無(self):	
+		平臺項目表.加外語資料(self.外語內容)
+		self.外語內容['種類'] = '語句'
+		self.assertRaises(ObjectDoesNotExist, 平臺項目表.找外語資料, self.外語內容)
+	def test_找外語資料語言腔口揣無(self):	
+		平臺項目表.加外語資料(self.外語內容)
+		self.外語內容['語言腔口'] = '噶哈巫'
+		self.assertRaises(ObjectDoesNotExist, 平臺項目表.找外語資料, self.外語內容)
+	def test_找外語資料外語語言揣無(self):
+		平臺項目表.加外語資料(self.外語內容)
+		self.外語內容['外語語言'] = '噶哈巫'
+		self.assertRaises(ObjectDoesNotExist, 平臺項目表.找外語資料, self.外語內容)
