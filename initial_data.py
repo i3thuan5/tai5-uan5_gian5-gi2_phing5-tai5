bd421000496ab975ceb7a027bbc4c982e6177021
薛丞宏 <ihcaoe@gmail.com>
2015-05-25 18:45:34 +0800

    提掉無效檔案

--- initial_data.py
+++
@@ -1,8 +0,0 @@
-from 臺灣言語資料庫.資料模型 import 版權表
-from 臺灣言語資料庫.資料模型 import 種類表
-from 臺灣言語資料庫.欄位資訊 import 字詞
-from 臺灣言語資料庫.欄位資訊 import 語句
-
-版權表.objects.get_or_create(版權='會使公開')
-種類表.objects.get_or_create(種類=字詞)
-種類表.objects.get_or_create(種類=語句)
