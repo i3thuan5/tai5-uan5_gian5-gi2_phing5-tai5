728ac4d23e5db6b21c33278af1a9227271dec9bc
薛丞宏 <ihcaoe@gmail.com>
2015-10-15 15:54:39 +0800

    配合新版django-allauth，改設定說明

--- tai5uan5_gian5gi2_phing5thai5/settings.py
+++ tai5uan5_gian5gi2_phing5thai5/settings.py
@@ -121,20 +121,27 @@ INSTALLED_APPS += (
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
 )
-TEMPLATE_CONTEXT_PROCESSORS = (
-    # Required by allauth template tags
-    "django.core.context_processors.request",
-    # allauth specific context processors
-    "allauth.account.context_processors.account",
-    "allauth.socialaccount.context_processors.socialaccount",
-    'django.contrib.auth.context_processors.auth',
-)
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     "django.contrib.auth.backends.ModelBackend",
     # `allauth` specific authentication methods, such as login by e-mail
     "allauth.account.auth_backends.AuthenticationBackend",
 )
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Required by allauth template tags
+                'django.contrib.auth.context_processors.auth',
+                # `allauth` needs this from django
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
 SOCIALACCOUNT_PROVIDERS = {
     'facebook': {
         'SCOPE': ['email', ],
