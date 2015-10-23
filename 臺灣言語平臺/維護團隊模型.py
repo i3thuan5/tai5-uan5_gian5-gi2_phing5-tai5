96bd872543f809ad12a2219fc4399570da249eee
薛丞宏 <ihcaoe@gmail.com>
2015-10-23 22:06:31 +0800

    用指令加sheet的json

--- 臺灣言語平臺/維護團隊模型.py
+++ 臺灣言語平臺/維護團隊模型.py
@@ -7,3 +7,13 @@ class 正規化sheet表(models.Model):
     client_email = models.CharField(blank=False, max_length=200)
     private_key = models.CharField(blank=False, max_length=4000)
     url = models.CharField(unique=True, max_length=200)
+
+    @classmethod
+    def 加sheet(cls, 語言腔口, client_email, private_key, url):
+        語言腔口物件 = 語言腔口表.objects.get_or_create(語言腔口=語言腔口)[0]
+        return cls.objects.create(
+            語言腔口=語言腔口物件,
+            client_email=client_email,
+            private_key=private_key,
+            url=url
+        )
