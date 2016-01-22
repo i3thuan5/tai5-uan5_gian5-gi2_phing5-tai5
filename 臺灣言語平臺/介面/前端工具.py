cf614929ede33ff89e3388c8ba39a95706ce73a2
薛丞宏 <ihcaoe@gmail.com>
2016-01-22 19:28:35 +0800

    解決兩個參數會漏勾第二個後壁的問題

--- 臺灣言語平臺/介面/前端工具.py
+++ 臺灣言語平臺/介面/前端工具.py
@@ -2,4 +2,8 @@ from django.http.response import HttpResponsePermanentRedirect
 
 
 def 重導向前端(request):
-    return HttpResponsePermanentRedirect(request.GET['網址'])
+    全部網址 = request.GET['網址']
+    for 參數, 內容 in request.GET.items():
+        if 參數!='網址':
+            全部網址 += '&{}={}'.format(參數, 內容)
+    return HttpResponsePermanentRedirect(全部網址)
--- 臺灣言語平臺/試驗/前端/test重導向前端試驗.py
+++ 臺灣言語平臺/試驗/前端/test重導向前端試驗.py
@@ -24,3 +24,16 @@ class 重導向前端試驗(TestCase):
             {'網址': 'http://itaigi.tw'}
         )
         self.assertEqual(回應.url, 'http://itaigi.tw')
+
+    def test_檢查網址有參數(self):
+        回應 = self.client.get(
+            '/導向',
+            {
+                '網址': '//itaigi.tw/k/kkk?%25E6%25BC%25A2%25E5%25AD%2597=AA',
+                '%25E9%259F%25B3%25E6%25A8%2599': 'BB'
+            }
+        )
+        self.assertEqual(
+            回應.url,
+            'http://itaigi.tw/k/kkk?%25E6%25BC%25A2%25E5%25AD%2597=AA&%25E9%259F%25B3%25E6%25A8%2599=BB'
+        )
