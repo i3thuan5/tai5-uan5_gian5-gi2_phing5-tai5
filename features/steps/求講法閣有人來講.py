b02d53e534972b961efa5eb95f9de3193241a9dc
薛丞宏 <ihcaoe@gmail.com>
2016-06-13 17:49:33 +0800

    定義行為檔

--- features/steps/求講法閣有人來講.py
+++ features/steps/求講法閣有人來講.py
@@ -1,8 +1,24 @@
-import json
-
 from behave import given, when, then, step
 
 
-@given('sss')
-def sss(context):
-    pass
+@then('無建議的詞內底無物件')
+def 無建議的詞內底無物件(context):
+    無建議的詞 = context.test.client.get('/平臺項目列表/揣無建議的外語').json()['列表']
+    context.test.assertEqual(len(無建議的詞), 0)
+
+
+@step('有人求 豬 的講法')
+def 有人求講法(context):
+    context.test.client.post('/平臺項目/加外語', {'外語資料': '豬'})
+
+
+@then('豬 無任何講法')
+def 講法內無揣著物件(context):
+    講法 = context.test.client.get('/平臺項目列表/揣列表', {'關鍵字': '豬'}).json()['列表']
+    context.test.assertEqual(len(講法), 0)
+
+
+@then('無建議的詞內底有 豬')
+def step_impl(context):
+    無建議的詞 = context.test.client.get('/平臺項目列表/揣無建議的外語').json()['列表']
+    context.test.assertEqual(無建議的詞[0]['外語資料'], '豬')
--- features/求講法閣有人來講.feature
+++ features/求講法閣有人來講.feature
@@ -1,11 +1,15 @@
 Feature: 求講法→我來講，要等維護團隊處理後，整個狀態才改變
 
-    Scenario: 一開始攏無人求講法
+    Scenario: 一開始攏無人求講法，無建議
        Then 無建議的詞內底無物件
        
-    Scenario: 有人問問題
+    Scenario: 一開始，無講法
        When 有人求 豬 的講法
-       Then 查 豬 無揣著物件
+       Then 豬 無任何講法
+       
+    Scenario: 有人問問題，就有無建議的詞
+       When 有人求 豬 的講法
+       Then 無建議的詞內底有 豬
 
     Scenario: 有人回答
        Given 有人求 豬 的講法
