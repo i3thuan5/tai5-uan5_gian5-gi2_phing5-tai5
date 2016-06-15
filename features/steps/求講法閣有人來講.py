6f9678b26507ae83eaea758fd1f3e70e74816b82
薛丞宏 <ihcaoe@gmail.com>
2016-06-15 13:13:59 +0800

    功能試驗加外語新詞

--- features/steps/求講法閣有人來講.py
+++ features/steps/求講法閣有人來講.py
@@ -20,6 +20,18 @@ def 講法內無揣著物件(context):
 
 
 @then('無建議的詞內底有 豬')
-def step_impl(context):
+def 無建議的詞內底有物件(context):
     無建議的詞 = context.test.client.get('/平臺項目列表/揣無建議的外語').json()['列表']
     context.test.assertEqual(無建議的詞[0]['外語資料'], '豬')
+
+
+@step('有人答 豬仔 的講法')
+def 有人答的講法(context):
+    外語回應 = context.test.client.post('/平臺項目/加外語', {'外語資料': '豬'})
+    回應 = context.test.client.post(
+        '/平臺項目/加新詞文本', {
+            '外語項目編號': 外語回應.json()['平臺項目編號'],  # 針對哪一個外語的母語文本
+            '文本資料': '媠',  # 錄製的文本檔，檔案型態
+        }
+    )
+    context.test.assertEqual(回應.status_code, 200)
