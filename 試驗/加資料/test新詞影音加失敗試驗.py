fbdec08376fa37c8b22b950b84fcfd2769484b0b
薛丞宏 <ihcaoe@gmail.com>
2016-06-18 17:18:13 +0800

    提掉無需要的import

--- 試驗/加資料/test新詞影音加失敗試驗.py
+++ 試驗/加資料/test新詞影音加失敗試驗.py
@@ -4,7 +4,6 @@ import json
 from unittest.mock import patch
 import wave
 
-from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase
 from 臺灣言語資料庫.資料模型 import 影音表
 from 臺灣言語資料庫.資料模型 import 外語表
