0b877b3aa84d33cac94871486d0762e49d8359d7
薛丞宏 <ihcaoe@gmail.com>
2016-06-29 16:53:13 +0800

    實作了 #104

--- 臺灣言語平臺/介面/揣外語請教條.py
+++ 臺灣言語平臺/介面/揣外語請教條.py
@@ -40,3 +40,17 @@ def 揣無建議的外語(request):
             '外語資料': 外語.外語資料,
         })
     return JsonResponse({'列表': 符合資料})
+
+
+def 揣按呢講外語請教條(request):
+    try:
+        關鍵字 = request.GET['關鍵字']
+    except:
+        return Json失敗回應({'錯誤': '無傳關鍵字'})
+    符合資料 = []
+    for 外語 in 平臺項目表.有按呢講法的外語表(關鍵字):
+        符合資料.append({
+            '外語項目編號': str(外語.平臺項目.編號()),
+            '外語資料': 外語.外語資料,
+        })
+    return JsonResponse({'列表': 符合資料})
--- 試驗/揣外語/test揣按呢講外語試驗.py
+++ 試驗/揣外語/test揣按呢講外語試驗.py
@@ -4,8 +4,7 @@ import json
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from 臺灣言語平臺.項目模型 import 平臺項目表
-from 臺灣言語資料庫.資料模型 import 來源表
-from 臺灣言語平臺.介面.揣外語請教條 import 揣外語請教條
+from 臺灣言語平臺.介面.揣外語請教條 import 揣按呢講外語請教條
 
 
 class 揣按呢講外語試驗(TestCase):
@@ -72,7 +71,7 @@ class 揣按呢講外語試驗(TestCase):
         新文本.設為推薦用字()
 #         前端輸入
         回應 = self.client.get('/平臺項目列表/揣按呢講列表', {
-            '關鍵字': '漂亮'
+            '關鍵字': '䖳'
         })
 #         前端回傳結果
         self.assertEqual(回應.status_code, 200)
@@ -81,7 +80,6 @@ class 揣按呢講外語試驗(TestCase):
             '外語項目編號': str(水母編號),
             '外語資料': '水母'
         }])
-        self.assertEqual(回應資料['其他建議'], [])
 
     def test_文本有兩組建議用字(self):
         水母編號 = self.資料庫加外語('水母')
