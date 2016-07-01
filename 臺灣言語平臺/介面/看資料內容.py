d775be5fb830efe020385c265760379644707bf5
thewayiam <twly.tw@gmail.com>
2016-07-01 23:32:56 +0800

    pep8

--- 臺灣言語平臺/介面/看資料內容.py
+++ 臺灣言語平臺/介面/看資料內容.py
@@ -59,6 +59,7 @@ def 看來源內容(request):
         pass
     return JsonResponse(來源內容)
 
+
 def 投票(request):
     try:
         平臺項目編號 = request.POST['平臺項目編號']
@@ -67,11 +68,13 @@ def 投票(request):
         return Json失敗回應({'錯誤': '沒有平臺項目的編號'})
     try:
         if decision == '按呢講好':
-            rows_affect = 平臺項目表.objects.filter(pk=平臺項目編號)\
-                                    .update(按呢講好=F('按呢講好') + 1)
+            rows_affect = 平臺項目表.objects\
+                                .filter(pk=平臺項目編號)\
+                                .update(按呢講好=F('按呢講好') + 1)
         elif decision == '按呢無好':
-            rows_affect = 平臺項目表.objects.filter(pk=平臺項目編號)\
-                                    .update(按呢無好=F('按呢無好') + 1)
+            rows_affect = 平臺項目表.objects\
+                                .filter(pk=平臺項目編號)\
+                                .update(按呢無好=F('按呢無好') + 1)
     except:
         return Json失敗回應({'錯誤': '這不是合法平臺項目的編號'})
     return JsonResponse({
