4774f37f7744c3e5f309cfaa45373dc07622dd9d
薛丞宏 <ihcaoe@gmail.com>
2016-07-02 08:00:35 +0800

    sql徙去模型內

--- 臺灣言語平臺/介面/看資料內容.py
+++ 臺灣言語平臺/介面/看資料內容.py
@@ -1,6 +1,5 @@
 from dateutil import tz
 from django.http.response import JsonResponse
-from django.db.models import F
 
 
 from 臺灣言語平臺.項目模型 import 平臺項目表
@@ -67,14 +66,7 @@ def 投票(request):
     except:
         return Json失敗回應({'錯誤': '沒有平臺項目的編號'})
     try:
-        if decision == '按呢講好':
-            rows_affect = 平臺項目表.objects\
-                                .filter(pk=平臺項目編號)\
-                                .update(按呢講好=F('按呢講好') + 1)
-        elif decision == '按呢無好':
-            rows_affect = 平臺項目表.objects\
-                                .filter(pk=平臺項目編號)\
-                                .update(按呢無好=F('按呢無好') + 1)
+        rows_affect = 平臺項目表.這句講了按怎(平臺項目編號, decision)
     except:
         return Json失敗回應({'錯誤': '這不是合法平臺項目的編號'})
     return JsonResponse({
--- 臺灣言語平臺/項目模型.py
+++ 臺灣言語平臺/項目模型.py
@@ -4,6 +4,7 @@ import json
 from django.conf import settings
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.db import models
+from django.db.models import F
 from django.db.models.query_utils import Q
 from django.utils import timezone
 
@@ -201,6 +202,12 @@ class 平臺項目表(models.Model):
     def 取消推薦用字(self):
         self.推薦用字 = False
         self.save()
+
+    @classmethod
+    def 這句講了按怎(cls, 平臺項目編號, decision):
+        return 平臺項目表.objects\
+            .filter(pk=平臺項目編號)\
+            .update(按呢講好=F(decision) + 1)
 
     @classmethod
     def _補預設欄位(cls, 內容):
