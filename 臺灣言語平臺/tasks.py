9c8c388767c8d0f58b5cb0d1162a1c656aba9cdb
薛丞宏 <ihcaoe@gmail.com>
2016-07-11 21:20:39 +0800

    celery就加shared_task佮設定schedule就好矣

--- 臺灣言語平臺/tasks.py
+++ 臺灣言語平臺/tasks.py
@@ -8,3 +8,8 @@ from 臺灣言語平臺.正規化團隊模型 import 正規化sheet表
 @shared_task
 def 新文本自資料庫加入sheet(平臺項目編號):
     正規化sheet表.文本加入sheet(平臺項目編號)
+
+
+@shared_task
+def 半瞑自sheets掠轉資料庫(self):
+    正規化sheet表.全部整理到資料庫()
