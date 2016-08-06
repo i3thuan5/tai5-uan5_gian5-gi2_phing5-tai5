988136f7206deae47919a5c35c383df164eb3e18
aweimeow <ccps960718456@gmail.com>
2016-08-06 12:12:35 +0800

    刻好管理page的初版前端

--- 臺灣言語平臺/介面/改資料.py
+++ 臺灣言語平臺/介面/改資料.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+from django.shortcuts import render
 from django.http.response import JsonResponse
 
 from 臺灣言語平臺.項目模型 import 平臺項目表
@@ -22,6 +23,23 @@ class 成功的json回應(JsonResponse):
         })
 
 #@login_required
+def 把測試資料藏起來_管理目錄(request):
+    資料列表 = 平臺項目表.無建議講法的外語表_隱藏詞條()
+
+    資料 = []
+    for data in 資料列表:
+        編號 = data.平臺項目.編號()
+        名稱 = data.平臺項目.資料()
+        愛藏起來 = data.平臺項目.愛藏起來
+        資料.append({
+            '編號': 編號, '名稱': 名稱, '愛藏起來': 愛藏起來
+        })
+
+    return render(request, '藏我很會.html', {
+        '資料': 資料,
+    })
+
+#@login_required
 def 把測試資料藏起來(request):
     try:
         資料編號 = int(request.POST['資料編號'].strip())
@@ -30,7 +48,3 @@ def 把測試資料藏起來(request):
     平臺項目表.把無建議的外語資料藏起來(資料編號)
     return 成功的json回應(資料編號)
 
-"""
-@login_required
-def 把測試資料藏起來目錄(request):
-"""
