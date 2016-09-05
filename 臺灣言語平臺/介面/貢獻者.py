9530793d2298342d173728a11ca0beb5c5a3f25b
aweimeow <ccps960718456@gmail.com>
2016-09-05 14:26:27 +0800

    以文本校對表做為貢獻依據

--- 臺灣言語平臺/介面/貢獻者.py
+++ 臺灣言語平臺/介面/貢獻者.py
@@ -3,26 +3,23 @@ import random
 from django.http.response import JsonResponse
 
 
-from 臺灣言語資料庫.資料模型 import 外語表
+from 臺灣言語資料庫.關係模型 import 文本校對表
 
 
 def 貢獻者表(request):
     # 2 = 臺灣閩南語常用詞辭典, 269 = 台文華文線頂辭典
-    except_contributor = [2, 269]
     contributor_dict = {}
-    for 詞語 in 外語表.objects.all():
-        # 過濾掉尚未有文本的詞條
-        if 詞語.翻譯文本.exists():
-            來源_id = 詞語.翻譯文本.first().文本.來源_id
-            來源名稱 = 詞語.翻譯文本.first().文本.來源.名
 
-            # 過濾掉由辭典提出的詞條
-            if 來源_id not in except_contributor:
-                if 來源_id not in contributor_dict:
-                    contributor_dict[來源_id] = dict()
-                    contributor_dict[來源_id]['名'] = 來源名稱
-                    contributor_dict[來源_id]['詞條'] = list()
-                contributor_dict[來源_id]['詞條'].append(詞語.外語資料)
+    for 文本 in 文本校對表.objects.all():
+        # 取出第一層
+        來源_id = 文本.舊文本.來源_id
+        來源名稱 = 文本.舊文本.來源.名
+        詞條名稱 = 文本.舊文本.來源外語.外語.外語資料
+
+        contributor_dict[來源_id] = dict()
+        contributor_dict[來源_id]['名'] = 來源名稱
+        contributor_dict[來源_id]['詞條'] = list()
+        contributor_dict[來源_id]['詞條'].append(詞條名稱)
 
     result = list()
 
