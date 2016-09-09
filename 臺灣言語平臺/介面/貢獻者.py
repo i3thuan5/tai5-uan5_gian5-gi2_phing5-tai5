570282339329239fabbfdcc3119f18656e89cfb3
aweimeow <ccps960718456@gmail.com>
2016-09-09 15:10:44 +0800

    fix coding style
    貢獻者.py:27:5: E265 block comment should start with '# '
    貢獻者.py:30:17: E128 continuation line under-indented for visual indent

--- 臺灣言語平臺/介面/貢獻者.py
+++ 臺灣言語平臺/介面/貢獻者.py
@@ -24,10 +24,8 @@ def 貢獻者表(request):
 
     result = list()
 
-    #for user in map(lambda x: x[1], sorted(contributor_dict.items(),
-    #                key=lambda x: len(x[1]['詞條']), reverse=True)):
     for user in sorted(contributor_dict.values(),
-                key=lambda x: len(x['詞條']), reverse=True):
+                       key=lambda x: len(x['詞條']), reverse=True):
         user['數量'] = len(user['詞條'])
         user['詞條'] = list(set(user['詞條']))
         if len(user['詞條']) > 10:
