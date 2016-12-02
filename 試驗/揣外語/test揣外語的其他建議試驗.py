ab70ebdcc929feca5b1357629be722448181fb4b
aweimeow <ccps960718456@gmail.com>
2016-12-02 17:06:56 +0800

    Fix PEP8 Coding Style
    ./試驗/揣外語/test揣外語的其他建議試驗.py:2:1: F401 'json' imported but unused
    ./試驗/揣外語/test揣外語的其他建議試驗.py:4:1: F401 'django.core.urlresolvers.resolve' imported but unused
    ./試驗/揣外語/test揣外語的其他建議試驗.py:112:9: F841 local variable '水母編號' is assigned to but never used
    ./試驗/揣外語/test揣外語的其他建議試驗.py:123:9: F841 local variable '握手編號' is assigned to but never used

--- 試驗/揣外語/test揣外語的其他建議試驗.py
+++ 試驗/揣外語/test揣外語的其他建議試驗.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import json
 
-from django.core.urlresolvers import resolve
 from django.test import TestCase
 from 臺灣言語平臺.項目模型 import 平臺項目表
 from 臺灣言語資料庫.資料模型 import 來源表
@@ -109,7 +107,7 @@ class 揣外語的其他建議試驗(TestCase):
         self.assertEqual(len(回應資料['其他建議']), 2)
 
     def test_其他建議愛有相關(self):
-        水母編號 = self.資料庫加外語('水母')
+        self.資料庫加外語('水母')
         水母腦編號 = self.資料庫加外語('水母腦')
         水母腦文本 = 平臺項目表.外語翻母語(水母腦編號, {
             '文本資料': '水母腦',
@@ -120,7 +118,7 @@ class 揣外語的其他建議試驗(TestCase):
             '文本資料': '水母國',
         })
         水母國文本.設為推薦用字()
-        握手編號 = self.資料庫加外語('握手')
+        self.資料庫加外語('握手')
 # 		前端輸入
         回應 = self.client.get('/平臺項目列表/揣列表', {
             '關鍵字': '水母'
