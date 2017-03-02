e4781360fd27a519751452f631a6bdb78d10e94b
薛丞宏 <ihcaoe@gmail.com>
2017-03-02 19:37:21 +0800

    攏改做https

--- 臺灣言語平臺/介面/工作.py
+++ 臺灣言語平臺/介面/工作.py
@@ -5,4 +5,4 @@ from 臺灣言語平臺.tasks import 半瞑自sheets掠轉資料庫
 
 def 正規化表馬上匯入資料庫(request):
     半瞑自sheets掠轉資料庫.delay()
-    return HttpResponseRedirect('http://itaigi.tw')
+    return HttpResponseRedirect('https://itaigi.tw')
