d457f663e2a8789a575628e9d33263a4d5d3f5d3
薛丞宏 <ihcaoe@gmail.com>
2017-05-13 08:15:01 +0800

    先加試驗

--- 試驗/揣外語/test揣無建議的外語試驗.py
+++ 試驗/揣外語/test揣無建議的外語試驗.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 from 臺灣言語平臺.項目模型 import 平臺項目表
 from 臺灣言語資料庫.資料模型 import 來源表
 from 臺灣言語平臺.介面.揣外語請教條 import 揣無建議的外語
+from django.core.exceptions import ValidationError
 
 
 class 揣無建議的外語試驗(TestCase):
@@ -21,7 +22,7 @@ class 揣無建議的外語試驗(TestCase):
         self.assertEqual(對應.func, 揣無建議的外語)
 
     def test_無外語物件(self):
-        回應 = self.client.get('/平臺項目列表/揣無建議的外語')
+        回應 = self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'new'})
         self.assertEqual(回應.status_code, 200)
         回應資料 = 回應.json()
         self.assertEqual(回應資料['列表'], [])
@@ -29,7 +30,7 @@ class 揣無建議的外語試驗(TestCase):
     def test_外語無物件(self):
         水母編號 = 平臺項目表.加外語資料(self.外語內容).編號()
 #         前端輸入
-        回應 = self.client.get('/平臺項目列表/揣無建議的外語')
+        回應 = self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'new'})
         self.assertEqual(回應.status_code, 200)
         回應資料 = 回應.json()
         self.assertEqual(回應資料['列表'], [{
@@ -44,7 +45,7 @@ class 揣無建議的外語試驗(TestCase):
         }
         平臺項目表.外語翻母語(水母編號, 䖳文本內容)
 #         前端輸入
-        回應 = self.client.get('/平臺項目列表/揣無建議的外語')
+        回應 = self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'new'})
         self.assertEqual(回應.status_code, 200)
         回應資料 = 回應.json()
         self.assertEqual(回應資料['列表'], [{
@@ -60,7 +61,53 @@ class 揣無建議的外語試驗(TestCase):
         母語 = 平臺項目表.外語翻母語(水母編號, 䖳文本內容)
         母語.設為推薦用字()
 #         前端輸入
-        回應 = self.client.get('/平臺項目列表/揣無建議的外語')
+        回應 = self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'new'})
         self.assertEqual(回應.status_code, 200)
         回應資料 = 回應.json()
         self.assertEqual(回應資料['列表'], [])
+
+    def test_照新問的排(self):
+        平臺項目表.加外語資料(self.外語內容)
+        try:
+            平臺項目表.加外語資料(self.外語內容)
+        except ValidationError:
+            pass
+        self.外語內容['外語資料'] = '頭腦'
+        平臺項目表.加外語資料(self.外語內容)
+
+        回應 = self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'new'})
+        self.assertEqual(回應.status_code, 200)
+        回應資料 = 回應.json()
+        self.assertEqual(len(回應資料['列表']), 2)
+        self.assertEqual(回應資料['列表'][0]['外語資料'], '頭腦')
+        self.assertEqual(回應資料['列表'][1]['外語資料'], '水母')
+
+    def test_照熱門排(self):
+        平臺項目表.加外語資料(self.外語內容)
+        try:
+            平臺項目表.加外語資料(self.外語內容)
+        except ValidationError:
+            pass
+        self.外語內容['外語資料'] = '頭腦'
+        平臺項目表.加外語資料(self.外語內容)
+
+        回應 = self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'hot'})
+        self.assertEqual(回應.status_code, 200)
+        回應資料 = 回應.json()
+        self.assertEqual(len(回應資料['列表']), 2)
+        self.assertEqual(回應資料['列表'][0]['外語資料'], '水母')
+        self.assertEqual(回應資料['列表'][1]['外語資料'], '頭腦')
+
+    def test_無參數就是照熱門排(self):
+        平臺項目表.加外語資料(self.外語內容)
+        try:
+            平臺項目表.加外語資料(self.外語內容)
+        except ValidationError:
+            pass
+        self.外語內容['外語資料'] = '頭腦'
+        平臺項目表.加外語資料(self.外語內容)
+
+        self.assertEqual(
+            self.client.get('/平臺項目列表/揣無建議的外語', {'排序': 'hot'}).json(),
+            self.client.get('/平臺項目列表/揣無建議的外語').json()
+        )
