addf2c8077d76a2ff7023cc63edf7ae1f8a66fb1
fafoy <ihcaoe@gmail.com>
2019-02-10 13:39:38 +0800

    Tsing-li ku e han-sik

--- 臺灣言語平臺/介面/貢獻者.py
+++ 臺灣言語平臺/介面/貢獻者.py
@@ -4,11 +4,7 @@ from django.db.models.expressions import F
 from django.http.response import JsonResponse
 
 
-from 臺灣言語資料庫.關係模型 import 文本校對表
-
-from 臺灣言語資料庫.資料模型 import 文本表
 from 臺灣言語平臺.辭典模型 import 華台對應表
-from 臺灣言語平臺.使用者模型 import 使用者表
 from 臺灣言語平臺.辭典模型 import 正規化表
 
 
@@ -43,14 +39,3 @@ def 正規化團隊表(request):
         .order_by('-數量', '名')
     )
     return JsonResponse({"名人": 名人})
-
-
-def 揣出會使的原始文本來源():
-    for 文本校對 in 華台對應表.objects.prefetch_related('舊文本__來源'):
-        yield 文本校對.舊文本.來源
-    for 文本 in (
-        文本表.objects
-        .filter(平臺項目__推薦用字=True, 來源校對資料__isnull=True)
-        .prefetch_related('來源')
-    ):
-        yield 文本.來源
