56fee8c7f13ef36749bc277fc51c7c1b141d3b60
fafoy <ihcaoe@gmail.com>
2019-02-12 20:12:24 +0800

    揣外語

--- 臺灣言語平臺/介面/揣外語請教條.py
+++ 臺灣言語平臺/介面/揣外語請教條.py
@@ -2,11 +2,11 @@ from django.db.models.query_utils import Q
 from django.http.response import JsonResponse
 
 
+
 from 臺灣言語平臺.介面.Json失敗回應 import Json失敗回應
-from 臺灣言語平臺.外語請教條 import 外語請教條
-from django.utils.datastructures import MultiValueDictKeyError
 from 臺灣言語平臺.管理.藏華語 import 華語管理表
-from 臺灣言語平臺.辭典模型 import 華台對應表
+from 臺灣言語平臺.辭典模型 import 華台對應表from django.db.models.expressions import F
+
 
 
 def 揣外語請教條(request):
@@ -17,28 +17,24 @@ def 揣外語請教條(request):
 
     符合資料 = []
 
-    for 外語 in 外語請教條.有建議講法的外語表().filter(外語資料=外語資料):
+    for 外語 in 華台對應表.有正規化的().filter(推薦華語=外語資料):
         符合資料.append({
-            '外語項目編號': str(外語.平臺項目.編號()),
-            '外語資料': 外語.外語資料,
+            '外語項目編號': str(外語.編號()),
+            '外語資料': 外語.推薦華語,
             '新詞文本': 外語.揣新詞文本(),
         })
 
     其他建議資料 = list(
-        文本表.objects
+        華台對應表.有正規化的()
         .exclude(
-            Q(來源外語__外語__外語資料=外語資料) |
-            Q(來源校對資料__舊文本__來源外語__外語__外語資料=外語資料)
-        )
-        .filter(平臺項目__推薦用字=True)
+            推薦華語=外語資料
+            )
         .filter(
-            Q(來源外語__外語__外語資料__contains=外語資料) |
-            Q(來源校對資料__舊文本__來源外語__外語__外語資料__contains=外語資料) |
-            Q(來源校對資料__舊文本__文本資料=外語資料) |
-            Q(文本資料=外語資料)
+            Q(推薦華語__contains=外語資料) |
+            Q(推薦漢字__contains=外語資料) 
         )
-        .order_by('文本資料', '音標資料')
-        .values('文本資料', '音標資料')
+        .order_by('推薦漢字', '推薦羅馬字')
+        .values(文本資料=F('推薦漢字)), 音標資料=F('推薦羅馬字'))
         .distinct()
     )
 
