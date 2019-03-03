dacac80e3ec39d5510de5a58f50efafc1ccacf89
Fafofafoy <ihcaoe@gmail.com>
2019-03-03 09:28:05 +0800

    鎖密碼

--- 臺灣言語平臺/migrations/0014_離跤手.py
+++ 臺灣言語平臺/migrations/0014_離跤手.py
@@ -18,6 +18,7 @@ def khok去使用者表(apps, schema_editor):
 def 設定使用者表ê名(apps, schema_editor):
     使用者表 = apps.get_model("臺灣言語平臺", "使用者表")
     for 使用者 in 使用者表.objects.select_related('來源'):
+        使用者.set_unusable_password()
         try:
             使用者.名 = 使用者.來源.名
             使用者.full_clean()
