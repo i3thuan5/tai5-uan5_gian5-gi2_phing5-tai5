843e9a75b9102aa09932f1051243c89d2dc7cd8c
Fafofafoy <ihcaoe@gmail.com>
2019-03-03 09:30:38 +0800

    Ing if 較簡單

--- 臺灣言語平臺/migrations/0014_離跤手.py
+++ 臺灣言語平臺/migrations/0014_離跤手.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 import django.utils.timezone
-from django.core.exceptions import ValidationError
 
 
 def khok去使用者表(apps, schema_editor):
@@ -19,13 +18,11 @@ def 設定使用者表ê名(apps, schema_editor):
     使用者表 = apps.get_model("臺灣言語平臺", "使用者表")
     for 使用者 in 使用者表.objects.select_related('來源'):
         使用者.set_unusable_password()
-        try:
+        if not 使用者表.objects.filter(名=使用者.名).exists():
             使用者.名 = 使用者.來源.名
-            使用者.full_clean()
             使用者.save()
-        except ValidationError:
+        else:
             使用者.名 = '{} {}'.format(使用者.來源.名, ':)')
-            使用者.full_clean()
             使用者.save()
 
 
