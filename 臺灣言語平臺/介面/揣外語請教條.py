3bf494fb46a62e1ab5f3a8d2185cfd16e5593f7b
niauah <fernandochen799@gmail.com>
2020-12-17 15:23:43 +0800

    Bian theh Hoa-gi pian-ho--ah.

--- 臺灣言語平臺/介面/揣外語請教條.py
+++ 臺灣言語平臺/介面/揣外語請教條.py
@@ -36,11 +36,10 @@ def 揣無建議的外語(request):
         照排 = ['-平臺項目__查幾擺', '-pk']
     符合資料 = []
     無建議的華語數量 = getattr(settings, 'BOKIANGI_HUAGI_MAX', 90)
-    for 外語值 in 外語請教條.無建議講法的外語表(照排, 無建議的華語數量).values('平臺項目__pk', '外語資料'):
-        符合資料.append({
-            '外語項目編號': str(外語值['平臺項目__pk']),
-            '外語資料': 外語值['外語資料'],
-        })
+    符合資料 = list(
+        外語請教條.
+        無建議講法的外語表(照排, 無建議的華語數量)
+        .values_list('外語資料', flat=True))
     return JsonResponse({'列表': 符合資料})
 
 
